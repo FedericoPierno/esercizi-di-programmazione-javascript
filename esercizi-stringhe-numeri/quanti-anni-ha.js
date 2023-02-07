4333851d304ab0bfd2bb874cad6886878d842483
FedericoPierno <piernofederico@gmail.com>
2023-02-07 16:54:11 +0100

    Update quanti-anni-ha.js
    ho completato l'esercizio tramite variabili e operatori di base

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -11,3 +11,15 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var CurrentYear = 2023;
+var birthDate = 1998;
+
+var PersonAge = 2023 - 1998;
+var OneHundred = 100 - PersonAge;
+console.log(`
+Questo esercizio l'ho completato nel ${CurrentYear}, sono nato nel ${birthDate}
+quindi ho ${PersonAge} anni e me ne mancano ${OneHundred} per arrivare a 100 anni.
+`) 
+/*questo esercizio l'ho completato nel 2023, 
+sono nato nel 1998 quindi ho 25 anni e me ne mancano 75 per arrivare a 100 anni.*/
